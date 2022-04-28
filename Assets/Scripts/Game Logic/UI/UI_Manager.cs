9cfe0773fa93de7e3296e80145e5d79d2c65bb6d
Alex Terradas <alex.terradas@gmail.com>
2022-04-28 22:13:39 +0200

    ui change color fixed finally

--- Assets/Scripts/Game Logic/UI/UI_Manager.cs
+++ Assets/Scripts/Game Logic/UI/UI_Manager.cs
@@ -36,14 +36,19 @@ public class UI_Manager : MonoBehaviour
 
     private void Update()
     {
-        Debug.DrawRay(m_arrowSpawnPoint.transform.position, m_arrowSpawnPoint.transform.forward * m_rayLength, Color.red);
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
 
-        if (Physics.Raycast(m_arrowSpawnPoint.transform.position, m_arrowSpawnPoint.transform.TransformDirection(Vector3.forward), 
-            out m_rayCastHit, m_rayLength) && m_rayCastHit.transform.gameObject.tag == "Enemy")
+        if (Physics.Raycast(ray, out hit))
         {
-
-            ChangeColorCrosshair(Color.red);
-
+            if (hit.transform.gameObject.tag == "Enemy")
+            {
+                ChangeColorCrosshair(Color.red);
+            }
+            else
+            {
+                ChangeColorCrosshair(Color.white);
+            }
         } else
         {
             ChangeColorCrosshair(Color.white);
