757cf26c5ad752601d46ddebf81d3951c14de329
Pau Sánchez <psanchezcairo60@gmail.com>
2022-04-28 18:11:59 +0200

    Sniper Enemy FINALLY

--- Assets/Scripts/Enemies/Sniper_Behaviour.cs
+++ Assets/Scripts/Enemies/Sniper_Behaviour.cs
@@ -9,16 +9,28 @@ public class Sniper_Behaviour : MonoBehaviour
     private float m_locationRadius;
 
     [SerializeField]
+    private GameObject m_projectile;
+
+    [SerializeField]
+    private float m_projectileSpeed = 100f;
+
+    [SerializeField]
     private float m_cooldownTime;
+
+    [SerializeField]
+    private float m_lockTime;
+
+    [SerializeField]
+    private float m_warningTime;
 
     [SerializeField]
     private Transform m_firePoint;
 
     [SerializeField]
-    private LayerMask m_shootLayerMask;
+    private LineRenderer m_ray;
 
     [SerializeField]
-    private float m_sniperDamage;
+    private LayerMask m_shootLayerMask;
 
     [SerializeField]
     private NavMeshAgent m_navMeshAgent;
@@ -32,10 +44,12 @@ public class Sniper_Behaviour : MonoBehaviour
     private int m_maxRad = 100;
 
     private Vector3 m_targetPos;
+    Vector3 m_finalPlayerPos;
 
     private string m_playerTag;
     private float m_timer;
-
+    private bool m_alreadyLocked;
+    private bool m_canLock;
 
     void Start()
     {
@@ -43,11 +57,25 @@ public class Sniper_Behaviour : MonoBehaviour
         m_targetPos = RandomNavmeshLocation(UtilsGyromitra.RandomNumber(m_minRad, m_maxRad));
         m_navMeshAgent.SetDestination(m_targetPos);
         m_radiusNearTarget = 2f;
+        m_ray.material.color = Color.blue;
     }
 
     void Update()
     {
-        // ShootSniper();
+        if (m_canLock)
+        {
+            LockOnPlayer();
+        }
+        else
+        {
+            m_ray.enabled = false;
+            m_timer -= Time.deltaTime;
+            if(m_timer <= 0)
+            {
+                m_canLock = true;
+                m_timer = 0f;
+            }
+        }
 
         if (Vector3.Distance(transform.position, m_targetPos) <= m_radiusNearTarget)
         {
@@ -55,7 +83,6 @@ public class Sniper_Behaviour : MonoBehaviour
             print(m_targetPos);
             m_navMeshAgent.SetDestination(m_targetPos);
         }
-
     }
     //Movement method
 
@@ -76,48 +103,55 @@ public class Sniper_Behaviour : MonoBehaviour
 
     //Shooting methods
 
-    private void ShootSniper()
+    private void LockOnPlayer()
     {
         GameObject l_player = UtilsGyromitra.FindInstanceWithinRadius(this.gameObject, m_playerTag, m_locationRadius);
 
         if (l_player != null)
         {
+            m_ray.enabled = true;
+
+            transform.LookAt(l_player.transform);
+            transform.eulerAngles = new Vector3(0f, transform.eulerAngles.y, transform.eulerAngles.z);
+            
             Vector3 l_playerPos = l_player.transform.position;
-            Vector3 l_enemyPos = gameObject.transform.position;
 
-            Vector3 l_direction = l_playerPos - l_enemyPos;
+            m_firePoint.forward = (l_playerPos - m_firePoint.position).normalized;
 
-            m_timer += Time.deltaTime;
-
-            if (m_timer >= m_cooldownTime)
+            if (m_timer >= m_warningTime && !m_alreadyLocked)
             {
-                Ray l_ray = new Ray(m_firePoint.position, l_direction);
-                RaycastHit l_raycastHit;
+                m_ray.material.color = Color.red;
+                m_alreadyLocked = true;
+            }          
 
-                if (Physics.Raycast(l_ray, out l_raycastHit, m_locationRadius, m_shootLayerMask))
-                {
-                    l_player.GetComponent<CharacterHP>().Damage(m_sniperDamage);
-
-                    print(l_player.tag);
-                    Debug.DrawLine(m_firePoint.position, l_raycastHit.point, Color.red);
-
-                    //yield WaitForSeconds(5);
-                    m_timer = 0;
-                }
-
-
+            Ray l_Ray = new Ray(m_firePoint.position, l_playerPos - m_firePoint.position);
+            if (Physics.Raycast(l_Ray, out RaycastHit l_RaycastHit, 10000f, m_shootLayerMask.value))
+            {
+                float l_distance = l_RaycastHit.distance;
+                m_ray.SetPosition(1, new Vector3(0.0f, 0.0f, l_distance));
             }
-        }
 
+            if (m_timer >= m_lockTime)
+            {
+                Vector3 l_finalDirection = l_playerPos - m_firePoint.position;
+                Shoot(l_finalDirection);
+                m_timer = m_cooldownTime;
+                m_ray.material.color = Color.blue;
+                m_alreadyLocked = false;
+                m_canLock = false;
+            }
+            m_timer += Time.deltaTime;
+        }
+        else
+        {
+            m_ray.enabled = false;
+        }
     }
 
-    private void ShootBazooka()
+    private void Shoot(Vector3 dir)
     {
-        GameObject l_player = UtilsGyromitra.FindInstanceWithinRadius(this.gameObject, m_playerTag, m_locationRadius);
-
-        if (l_player != null)
-        {
-
-        }
+        GameObject l_projectile = Instantiate(m_projectile, m_firePoint.position, m_firePoint.rotation);
+        Rigidbody rb = l_projectile.GetComponent<Rigidbody>();
+        rb.velocity = dir.normalized * m_projectileSpeed;
     }
 }
