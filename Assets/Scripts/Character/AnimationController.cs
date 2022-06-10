766f46adbe224015ae9f3574b6510a89fb6044b1
Touchme <jose.canovas98@gmai.com>
2022-06-10 03:52:38 +0200

    cambios animator seiri

--- Assets/Scripts/Character/AnimationController.cs
+++ Assets/Scripts/Character/AnimationController.cs
@@ -7,7 +7,13 @@ public class AnimationController : MonoBehaviour
     public Animator m_Animator;
 
     [SerializeField]
-    private float m_WeightSmooth = 1.0f;
+    private float m_WeightSmoothAim = 1.0f;
+    [SerializeField]
+    private float m_WeightSmoothHolster = 1.0f;
+
+    private bool m_shoot = false;
+
+    private float m_timer = 0.0f;
 
     static float t = 0.0f;
     static float s = 0.0f;
@@ -37,17 +43,32 @@ public class AnimationController : MonoBehaviour
     {
         if (_isAiming)
         {
+            m_timer += Time.deltaTime;
             s = 0;
-            t += m_WeightSmooth * Time.deltaTime;
+            t += m_WeightSmoothAim * Time.deltaTime;
             m_Animator.SetLayerWeight(1, Mathf.Lerp(0f, 1f, t));
-            m_Animator.SetLayerWeight(2, Mathf.Lerp(0f, 1f, t));
+            //m_Animator.SetLayerWeight(2, Mathf.Lerp(0f, 1f, t));
+            m_shoot = true;
+        }
+        else if (!_isAiming && m_shoot)
+        {
+            if (m_timer < 0.2)
+            {
+                m_Animator.SetTrigger("shootShort");
+            }
+            else
+            {
+                m_Animator.SetTrigger("shootLong");
+            }
+            m_shoot = false;
+            m_timer = 0;
         }
         else
         {
             t = 0;
-            s += m_WeightSmooth * Time.deltaTime;
+            s += m_WeightSmoothHolster * Time.deltaTime;
             m_Animator.SetLayerWeight(1, Mathf.Lerp(1f, 0f, s));
-            m_Animator.SetLayerWeight(2, Mathf.Lerp(1f, 0f, s));
+            //m_Animator.SetLayerWeight(2, Mathf.Lerp(1f, 0f, s));
         }
     }
 
@@ -58,16 +79,16 @@ public class AnimationController : MonoBehaviour
 
     public void AnimationShootShort()
     {
-        m_Animator.SetTrigger("shootShort");
-        m_Animator.SetLayerWeight(1, 0.5f);
-        m_Animator.SetLayerWeight(2, 1);
+        //m_Animator.SetTrigger("shootShort");
+        //m_Animator.SetLayerWeight(1, 1f);
+        //m_Animator.SetLayerWeight(2, 1);
     }
 
     public void AnimationShootLong()
     {
         m_Animator.SetTrigger("shootLong");
-        m_Animator.SetLayerWeight(1, 0.5f);
-        m_Animator.SetLayerWeight(2, 1);
+        //m_Animator.SetLayerWeight(1, 1f);
+        //m_Animator.SetLayerWeight(2, 1);
     }
 
 }
