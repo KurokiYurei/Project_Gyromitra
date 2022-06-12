8c7b6636bacf8c0dc5da007d3957300b918909ce
KurokiYurei <62447760+KurokiYurei@users.noreply.github.com>
2022-06-12 23:48:19 +0200

    Steps updated

--- Assets/Scripts/Character/PlayerSounds.cs
+++ Assets/Scripts/Character/PlayerSounds.cs
@@ -10,36 +10,25 @@ public class PlayerSounds : MonoBehaviour
     private Transform m_soundEmitter;
 
     [SerializeField]
-    private EventInstance m_eventPasesDeMerda;
+    private EventInstance m_eventStep;
 
     private void Awake()
     {
-        m_eventPasesDeMerda = FMODUnity.RuntimeManager.CreateInstance("event:/Personatge/12 - Passes terra");
-    }
-
-    public void PassesDeMerda()
-    {
-        //playSound()
+        m_eventStep = FMODUnity.RuntimeManager.CreateInstance("event:/Personatge/12 - Passes terra");
     }
 
     public void StepLeft()
     {
+        UtilsGyromitra.stopSound(m_eventStep);
         print("left");
+        UtilsGyromitra.playSound(m_eventStep, m_soundEmitter);
+
     }    
 
     public void StepRight()
     {
+        UtilsGyromitra.stopSound(m_eventStep);
         print("right");
-    }
-
-    private void playSound(EventInstance l_event)
-    {
-        l_event.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(m_soundEmitter.transform));
-        l_event.start();
-    }
-
-    private void stopSound(EventInstance l_event)
-    {
-        l_event.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        UtilsGyromitra.playSound(m_eventStep, m_soundEmitter);
     }
 }
