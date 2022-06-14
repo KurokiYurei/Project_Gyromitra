ce06a2b056fc432fd0008aa185ef9702322557a7
Touchme <jose.canovas98@gmai.com>
2022-06-15 00:45:44 +0200

    SMOKE SMOKE THE SHIT OUT OF HERE

--- Assets/Scripts/Character/PlayerEffects.cs
+++ Assets/Scripts/Character/PlayerEffects.cs
@@ -6,23 +6,38 @@ public class PlayerEffects : MonoBehaviour
 {
     [SerializeField]
     private GameObject m_groundRunEffect;
+    [SerializeField]
+    private GameObject m_jumpEffect;
 
     [SerializeField]
     private GameObject m_leftFootFirepoint;
     [SerializeField]
     private GameObject m_rightFootFirepoint;
+    [SerializeField]
+    private GameObject m_centerFootFirepoint;
 
     public void StepLeft()
     {
-        Debug.Log("left");
         GameObject _smokeVFX = Instantiate(m_groundRunEffect, m_leftFootFirepoint.transform.position, m_leftFootFirepoint.transform.rotation) as GameObject;
         Destroy(_smokeVFX, 2);
     }
 
     public void StepRight()
     {
-        Debug.Log("right");
         GameObject _smokeVFX = Instantiate(m_groundRunEffect, m_rightFootFirepoint.transform.position, m_rightFootFirepoint.transform.rotation) as GameObject;
         Destroy(_smokeVFX, 2);
+    }
+
+    public void JumpImpulse()
+    {
+        GameObject _smokeVFX = Instantiate(m_jumpEffect, m_centerFootFirepoint.transform.position, m_centerFootFirepoint.transform.rotation) as GameObject;
+        Destroy(_smokeVFX, 2);
+    }
+
+    public void LandingEvent()
+    {
+        JumpImpulse();
+        StepLeft();
+        StepRight();
     }
 }
